2d409089aefdc76e6464d4bcf53d55e3a9fb3bdc
Dylan  Rhoads <dylan.rhoads@valiantys.com>
2025-03-15 21:47:45 -0600

    Week 1 excersieses

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 1110);
+        Console.WriteLine("Hello, I have a magic number that I have selected! Make your gues and I will tell you if you are High or Low.");
+        int guess = 0;
+        while (guess != number) {
+            Console.Write("Enter your guess: ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > number) {
+                Console.WriteLine("Too high! Try again.");
+            }
+            else if (guess < number) {
+                Console.WriteLine("Too low! Try again.");
+                }
+            else {
+                Console.WriteLine("Congratulations! You guessed the correct number!");
+            }
+        }
     }
 }
